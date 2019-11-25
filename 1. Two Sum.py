82dbc4dcee246e9641f9f552252a34e0f399c514
Danny Kuo <danny.kuo25@gmail.com>
2019-11-24 21:11:19 -0500

    Update 1. Two Sum.py

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -2,7 +2,7 @@
 # hashmap
 # time: O(N), space: O(N)
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, nums, target):
         n = len(nums)
         dic = {}      
         for i in range(n):
@@ -12,12 +12,14 @@ class Solution:
                 return [i, dic[target - nums[i]]]
                 
 # one iteration
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+class Solution2:
+    def twoSum(self, nums, target):
         n = len(nums)
         dic = {}      
         for i in range(n):
-            dic[nums[i]] = i
-        for i in range(n):
-            if target - nums[i] in dic and dic[target - nums[i]] != i:
+            if target - nums[i] not in dic:
+                dic[nums[i]] = i
+            else:    
                 return [i, dic[target - nums[i]]]
+
+print(Solution2().twoSum([3,3], 6))
